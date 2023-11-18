a499adeca591bce2c0d0879415b546d19914bba5
Antiquete <antiquete@proton.me>
2023-11-18 11:07:31 +0530

    Degoogle: Switch to GrapheneOS captive portals
    Change-Id: I2d9f1479fa92005b0d7865ff6c5f929f3e586638

--- src/com/android/captiveportallogin/CaptivePortalLoginActivity.java
+++ src/com/android/captiveportallogin/CaptivePortalLoginActivity.java
@@ -117,7 +117,7 @@ public class CaptivePortalLoginActivity extends Activity {
     private static final int SOCKET_TIMEOUT_MS = 10000;
     public static final String HTTP_LOCATION_HEADER_NAME = "Location";
     private static final String DEFAULT_CAPTIVE_PORTAL_HTTP_URL =
-            "http://connectivitycheck.gstatic.com/generate_204";
+            "http://connectivitycheck.grapheneos.network/generate_204";
     public static final String DISMISS_PORTAL_IN_VALIDATED_NETWORK =
             "dismiss_portal_in_validated_network";
     // This should match the FileProvider authority specified in the app manifest.
